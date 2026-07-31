3effe2975dff2f2375fad9e953f545aebb07b249
vdescent <timotiusryjaska@gmail.com>
2026-07-31 17:29:59 +0700

    feat: add post movie to db

--- src/app/pages/admin/page.tsx
+++ src/app/pages/admin/page.tsx
@@ -1,12 +1,24 @@
 "use client"
 import React, {useEffect, useState} from 'react'
-import Link from 'next/link';
+import { db } from '@/app/firebase/Init';
+import { setDoc, addDoc, collection, serverTimestamp, getDoc, doc } from 'firebase/firestore';
 // CRD no update
 
 export default function AdminPage() {
   const apiKey = process.env.NEXT_PUBLIC_OMDB_API_KEY;
   const [keyword, setKeyword] = useState("");
   const [movies, setMovies] = useState([]);
+  const [form, setForm] = useState({
+    imdbID:"",
+    title:"",
+    rating:"",
+    genre:"",
+    duration:"",
+    releaseYear:"",
+    actors:"",
+    plot:"",
+    image:"",
+  })
 
   useEffect(()=>{
     if(keyword.trim() === ""){
@@ -30,36 +42,108 @@ export default function AdminPage() {
     return ()=> clearTimeout(timer);
   }, [keyword]);
 
-  // const res = await fetch(
-  //   `https://www.omdbapi.com/?apikey=${apiKey}&s=Batman`
-  // );
+  const handleSelectMovie = async (imdbID) =>{
+    const res = await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&i=${imdbID}`);
 
-  // console.log();
+    const data = await res.json();
+
+    if(data.Response === "True"){
+      setForm({
+        imdbID: data.imdbID,
+        title: data.Title,
+        rating: data.imdbRating,
+        genre: data.Genre,
+        duration: data.Runtime,
+        releaseYear: data.Year,
+        actors: data.Actors,
+        plot: data.Plot,
+        image: data.Poster,
+      })
+    }
+  }
+
+  const handleAddMovie = async ()=>{
+    try{
+      const movieRef = doc(db, "movie-list", form.imdbID);
+      const movieSnap = await getDoc(movieRef);
+
+      if(movieSnap.exists()){
+        await setDoc(
+          movieRef, {...form, lastUpdateOn:serverTimestamp()}, {merge:true}
+        );
+        alert(`Movie already in db and updated successfully`);
+      } else {
+        await setDoc(movieRef, {
+          ...form, 
+          firstAddedOn: serverTimestamp(),
+          lastUpdateOn: serverTimestamp(),
+        });
+        alert("Movie added successfully");
+      }
+
+      setForm({
+        imdbID:"",
+        title: "",
+        rating: "",
+        genre: "",
+        duration: "",
+        releaseYear: "",
+        actors: "",
+        plot: "",
+        image: "",
+      });
+
+      setKeyword("");
+      setMovies([]);
+
+    } catch(error){
+      console.error(error);
+      alert("Gagal menambahkan movie");
+    }
+  }
+
 
   return (
     <div>
       <h1 className='text-4xl'>Add Movie</h1>
-      <h3>Search Movie You Want To Add: </h3>
+      <h2>Search Movie You Want To Add: </h2>
       <input type="text" className='bg-amber-200' placeholder='ex: batman' value={keyword} onChange={(e)=> setKeyword(e.target.value)}/>
       <div className='mt-4'>
         {movies.map((movie)=>(
-          <div key={movie.imdbID}>
+          <div key={movie.imdbID} onClick={()=> handleSelectMovie(movie.imdbID)} className='cursor-pointer border p-2 hover:bg-gray-200'>
             <img src={movie.Poster} alt={movie.Title} width={100} />
             <p>{movie.Title}</p>
             {/* link ke fetch detail movie */}
           </div>
         ))}
       </div>
+      <h3>Title: </h3>
+      <input type="text" value={form.title} onChange={(e)=>setForm({...form, title: e.target.value})}/>
+      <h3>Ratings: </h3>
+      <input type="text" value={form.rating} onChange={(e)=> setForm({...form, rating: e.target.value})}/>
+      <h3>Genre: </h3>
+      <input type="text" value={form.genre} onChange={(e)=> setForm({...form, genre: e.target.value})}/>
+      <h3>Duration: </h3>
+      <input type="text" value={form.duration} onChange={(e)=> setForm({...form, duration: e.target.value})}/>
+      <h3>Release Year: </h3>
+      <input type="text" value={form.releaseYear} onChange={(e)=> setForm({...form, releaseYear:e.target.value})}/>
+      <h3>Actors: </h3>
+      <input type="text" value={form.actors} onChange={(e)=> setForm({...form, actors:e.target.value})}/>
+      <h3>Plot: </h3>
+      <input type="text" value={form.plot} onChange={(e)=> setForm({...form, plot:e.target.value})}/>
+      <h3>Image Link: </h3>
+      <input type="text" disabled placeholder='This will fill automatically if movie is available' value={form.image} onChange={(e)=> setForm({...form, image:e.target.value})}/>
 
 
-      <button className='bg-green-300'
+      <button className='bg-green-300 cursor-pointer hover:bg-green-400 rounded-lg p-2'
+      onClick={handleAddMovie}
       >Add New Movie</button>
       {/* Trus ada pop up dan isi disitu saja */}
       <h1 className='text-4xl'>Movie list</h1>
       <div>
         <h1>ini img</h1>
         <p>Ini judul</p>
-        <p>Year : </p>
+        <p>Duration : </p>
         <button>Remove</button>
       </div>
     </div>
